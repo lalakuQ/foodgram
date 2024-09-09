2645d80e02750b3c97606eba674389977a08123d
lalakuQ <vdyugay2004@gmail.com>
2024-09-09 20:29:45 +0500

    init

---
+++ backend/recipes/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    username = models.CharField()
+    first_name = models.CharField()
+    last_name = models.CharField()
+    email = models.EmailField()
+    avatar = models.ImageField()
+    bookmarked_recipes = models.ManyToManyField('Recipe',
+                                                related_name='users')
+
+
+class Follower(models.Model):
+    user = models.ForeignKey(User,
+                             related_name='following',)
+    following_user = models.ForeignKey(User,
+                                       related_name='followers')
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'following_user'],
+                name='unique_followers'
+            )
+        ]
+
+
+class Recipe(models.Model):
+    author = models.ForeignKey('User',
+                               related_name='recipes')
+    name = models.CharField(unique=True)
+    image = models.ImageField()
+    description = models.TextField()
+    ingredients = models.ManyToManyField(
+        'Ingredient',
+        on_delete=models.CASCADE,
+        related_name='recipes'
+    )
+    tag = models.ManyToManyField(
+        'Tag',
+        related_name='recipes',
+    )
+    prep_time = models.IntegerField()
+
+
+class Tag(models.Model):
+    name = models.CharField(unique=True)
+    slug = models.SlugField(unique=True)
+
+
+class Ingredient(models.Model):
+    name = models.CharField(unique=True)
+    unit = models.CharField()
