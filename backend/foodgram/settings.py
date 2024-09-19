c6c580e8b8bbb897cfe331e8525ae8579bd5b2df
lalakuQ <vdyugay2004@gmail.com>
2024-09-19 23:43:08 +0500

    update vars

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'foodgram'),
+        'NAME': os.getenv('POSTGRES_DB', 'django_db'),
         'USER': os.getenv('POSTGRES_USER', 'django_user'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'django_password'),
         'HOST': os.getenv('DB_HOST', 'db'),
